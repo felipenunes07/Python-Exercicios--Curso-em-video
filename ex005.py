b805b103a8466cf27f8b42b2f1afbba4ca48227c
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-01-09 10:56:55 -0300

    Exercicios Python

--- ex005.py
+++ ex005.py
@@ -1,2 +1,5 @@
+## Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e sucessor e seu antecessor.##
+
+
 n1 = int(input('Digite um número:'))
 print('Analisando o valor {} o seu antecessor é {} e o seu sucessor é {}'.format(n1,n1-1,n1+1))
