40135922e3995e5d4a61d37ceed9b98703b68a76
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-01-09 10:59:46 -0300

    Exercicios Python

--- ex007.py
+++ ex007.py
@@ -1,3 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.##
+
+
 p1 = float(input('Primeira nota do aluno:'))
 p2 = float(input('Segunda nota do aluno:'))
 print('A média entre {:.1f} e {:.1f} é {:.1f}.'.format(p1,p2,(p1+p2)/2))
