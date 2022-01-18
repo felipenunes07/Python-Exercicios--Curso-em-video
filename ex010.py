ef0b9570ffdf021c894341742c4c77558a2a134b
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-01-18 14:01:49 -0300

    Exercicios Python

--- ex010.py
+++ ex010.py
@@ -1,2 +1,5 @@
+## Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere $ 3.27##
+
+
 n = float(input('Quanto dinheiro você tem na carteira: R$'))
 print('Com R${} você pode comprar U${:.2f}. '.format(n,(n/3.27)))
