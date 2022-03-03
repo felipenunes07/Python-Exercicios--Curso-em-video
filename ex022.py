4f04634136b3c6a547e74c6a56dacee90c138afc
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-03-02 23:42:12 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,3 +1,8 @@
+''' Crie um programa que leia o nome completo de uma pessoa e mostre: 
+- O nome com todas as letras maiúsculas e minúsculas.
+- Quantas letras ao todo (sem considerar espaços).
+- Quantas letras tem o primeiro nome. '''
+
 nome = input('Digite o seu nome completo: ').strip()
 print('Seu nome em maiusculas é {}.'.format(nome.upper()))
 print('Seu nome em minuscula é {}.'.format(nome.lower()))
