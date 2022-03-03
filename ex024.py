3487ded7278bf612bd17e06c95640964a1a30b57
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-03-02 23:43:40 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,2 +1,4 @@
+'''Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".'''
+
 cid = str(input('Em que cidade vocÊ nsceu: ')).strip()
 print(cid[:5].upper() == 'SANTO')
