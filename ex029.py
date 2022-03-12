3c0bf2ce04fe3e06af69493095569ee7c63675a5
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-03-11 22:07:14 -0300

    Update ex029.py

--- ex029.py
+++ ex029.py
@@ -1,3 +1,7 @@
+#Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
+#mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+
 v = int(input('Qual é a velocidade atual do carro: '))
 if v > 80:
     print('\033[31m MULTADO! Você excedeu o limite permitido que é de 80Km/h')
