409560b5f43b663de8536f52f10dd316961b042e
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-03-12 23:53:40 -0300

    Update ex030.py

--- ex030.py
+++ ex030.py
@@ -1,3 +1,5 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
 n = int(input('Me diga um número qualquer: '))
 if n % 2 == 0:
     print('o Numero {} é \033[34mPar '.format(n))
