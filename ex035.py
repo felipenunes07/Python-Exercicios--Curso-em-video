741eb172c09e996994b6e9b29b2d239f2c452b76
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-03-15 22:56:13 -0300

    Update ex035.py

--- ex035.py
+++ ex035.py
@@ -1,3 +1,5 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
 print('-='*20)
 print('Analisador de Triângulo')
 print('-='*20)
